e33800308e55d729655f41565737844e664088a9
Red <Smartlizardpro@gmail.com>
2021-10-07 19:13:50 -0700

    Add Sound Cache, speed up loading of duplicate sfx

--- Graphics/SoundEngine.java
+++ Graphics/SoundEngine.java
@@ -3,19 +3,36 @@ package Graphics;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.sound.sampled.*;
 
 public class SoundEngine{
+
+    static Map<String, AudioInputStream> clips = new HashMap<String, AudioInputStream>();
+
     public static synchronized void playSound(final String location) {
         new Thread(new Runnable() {
-            // The wrapper thread is unnecessary, unless it blocks on the
-            // Clip finishing; see comments.
             public void run() {
                 try {
                     Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = 
-                        AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(new File(location))));
+                    AudioInputStream inputStream; 
+                    if(clips.containsKey(location)){
+                        inputStream = clips.get(location);
+                        inputStream.reset();
+                    }
+                    else {
+                        inputStream = AudioSystem.getAudioInputStream(
+                            new BufferedInputStream(
+                                new FileInputStream(
+                                    new File(location)
+                                )
+                            )
+                        );
+                        clips.put(location, inputStream);
+                        inputStream.mark(10000000);   
+                    }
                     clip.open(inputStream);
                     clip.start();
                 } catch (Exception e) {
